0e5e1ac56c348d8e0fb3ab63dba7162addd8cfc9
Flex Escola <bruno.lobao@outlook.com>
2020-08-01 16:08:31 -0400

    Navageção Pessoas

---
+++ resources/views/pessoa/create.blade.php
@@ -0,0 +1,11 @@
+<h1>Formulario de Pessoa</h1>
+
+<form action="/pessoa" method="post">
+    <input type="text"name="nome">
+    <input type="text" name="telefone">
+    <input type="text"name="email">
+    <button type="submit">Salvar</button>
+</form>
+<br>
+
+<a href="/pessoas">Voltar</a>
